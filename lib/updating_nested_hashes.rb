ed91a9e6cfcd6ca73689d5ca8311490d6c85c843
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-09-21 18:45:53 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -46,7 +46,7 @@ def add_characters
       }
    }
    
-  epic_tragedy[:additional_characters] = [{name: "Prince"}]
+  epic_tragedy[:additional_characters] = [{name: "Prince Escalus"}, {name: "Apothecary"}]
 
    # Write code below that does the following programmatically
    #
